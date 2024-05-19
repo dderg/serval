5c900c57e670d598a2612c95ea0481f3784c556a
Dmitry Butyugin <dmbutyugin@google.com>
2024-05-19 22:23:00 +0100

    input_shaper: Added smooth_zvd_ei smoother
    Signed-off-by: Dmitry Butyugin <dmbutyugin@google.com>

--- klippy/extras/shaper_calibrate.py
+++ klippy/extras/shaper_calibrate.py
@@ -18,7 +18,19 @@ MAX_SHAPER_FREQ = 215.0
 
 TEST_DAMPING_RATIOS = [0.075, 0.1, 0.15]
 
-AUTOTUNE_SHAPERS = ["zv", "mzv", "ei", "2hump_ei", "3hump_ei"]
+AUTOTUNE_SHAPERS = [
+    "smooth_zv",
+    "smooth_mzv",
+    "smooth_ei",
+    "smooth_2hump_ei",
+    "smooth_zvd_ei",
+    "smooth_si",
+    "zv",
+    "mzv",
+    "ei",
+    "2hump_ei",
+    "3hump_ei",
+]
 
 ######################################################################
 # Frequency response calculation and shaper auto-tuning
