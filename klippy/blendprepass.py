2994b4bacea0981003cdb5161a289cbaefe474ab
Danila Dergachev <dderg@protonmail.com>
2026-04-17 23:33:18 +0200

    blendprepass: module + test scaffolding

---
+++ klippy/blendprepass.py
@@ -0,0 +1,35 @@
+# klippy/blendprepass.py
+# Copyright (C) 2026
+# This file may be distributed under the terms of the GNU GPLv3 license.
+from __future__ import annotations
+
+import logging
+import math
+
+from . import blendmath
+
+
+class CollinearCollapser:
+    """Naive-CAM collinearity prepass. See
+    docs/superpowers/specs/2026-04-17-naive-cam-prepass-design.md for rationale.
+    """
+
+    def __init__(self, toolhead, move_cls):
+        self._toolhead = toolhead
+        self._move_cls = move_cls
+        self._chain = []
+        self.tolerance = 25e-3
+        self.max_chain = 100
+        self.epm_rel = 1e-2
+        self.f_rel = 1e-6
+        self.min_seg_len = 1e-9
+        self.t_eps = 1e-9
+
+    def feed(self, move):
+        return []
+
+    def flush(self):
+        return []
+
+    def reset(self):
+        self._chain = []
---
+++ test/test_blendprepass.py
@@ -0,0 +1,88 @@
+# test/test_blendprepass.py
+import math
+import random
+
+import pytest
+
+from klippy import blendprepass
+
+
+class _FakeCheckMove:
+    def __init__(self, exc=None):
+        self.calls = []
+        self._exc = exc
+
+    def check_move(self, move):
+        self.calls.append(move)
+        if self._exc is not None:
+            raise self._exc
+
+
+class _FakeToolhead:
+    def __init__(self, **overrides):
+        self.max_velocity = overrides.get("max_velocity", 500.0)
+        self.max_accel = overrides.get("max_accel", 10000.0)
+        self.max_accel_to_decel = overrides.get("max_accel_to_decel", 10000.0)
+        self.junction_deviation = overrides.get("junction_deviation", 0.01)
+        self.kin = _FakeCheckMove()
+        self.extruder = _FakeCheckMove()
+
+
+class _FakeMove:
+    """Reimplements klippy.toolhead.Move.__init__ without pulling pyserial."""
+
+    def __init__(self, toolhead, start_pos, end_pos, speed):
+        self.toolhead = toolhead
+        self.start_pos = tuple(start_pos)
+        self.end_pos = tuple(end_pos)
+        self.accel = toolhead.max_accel
+        self.junction_deviation = toolhead.junction_deviation
+        self.timing_callbacks = []
+        velocity = min(speed, toolhead.max_velocity)
+        self.is_kinematic_move = True
+        axes_d = [end_pos[i] - start_pos[i] for i in (0, 1, 2, 3)]
+        self.axes_d = axes_d
+        move_d = math.sqrt(sum(d * d for d in axes_d[:3]))
+        if move_d < 0.000000001:
+            self.end_pos = (
+                start_pos[0],
+                start_pos[1],
+                start_pos[2],
+                end_pos[3],
+            )
+            axes_d[0] = axes_d[1] = axes_d[2] = 0.0
+            move_d = abs(axes_d[3])
+            inv_move_d = 1.0 / move_d if move_d else 0.0
+            self.accel = 99999999.9
+            velocity = speed
+            self.is_kinematic_move = False
+        else:
+            inv_move_d = 1.0 / move_d
+        self.move_d = move_d
+        self.axes_r = [d * inv_move_d for d in axes_d]
+        self.min_move_t = move_d / velocity if velocity else 0.0
+        self.max_start_v2 = 0.0
+        self.max_cruise_v2 = velocity ** 2
+        self.delta_v2 = 2.0 * move_d * self.accel
+        self.max_smoothed_v2 = 0.0
+        self.smooth_delta_v2 = 2.0 * move_d * toolhead.max_accel_to_decel
+        self.next_junction_v2 = 999999999.9
+
+    def limit_speed(self, speed, accel):
+        speed2 = speed ** 2
+        if speed2 < self.max_cruise_v2:
+            self.max_cruise_v2 = speed2
+            self.min_move_t = self.move_d / speed if speed else 0.0
+        self.accel = min(self.accel, accel)
+        self.delta_v2 = 2.0 * self.move_d * self.accel
+        self.smooth_delta_v2 = min(self.smooth_delta_v2, self.delta_v2)
+
+
+def _collapser(toolhead=None):
+    th = toolhead or _FakeToolhead()
+    return blendprepass.CollinearCollapser(th, move_cls=_FakeMove)
+
+
+def test_construct_and_flush_empty():
+    c = _collapser()
+    assert c.flush() == []
