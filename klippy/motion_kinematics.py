26851da1281d9ab59ac402048700ffc6aab50e87
Danila Dergachev <dderg@protonmail.com>
2026-06-13 13:14:09 +0200

    feat(klippy): motion_kinematics module — [kinematics] section, _LinearKinematics (stepper lanes)

---
+++ klippy/motion_kinematics.py
@@ -0,0 +1,227 @@
+from . import stepper
+
+DRIVE_CHOICES = {"stepper": "stepper", "servo": "servo"}
+
+_KIN_COREXY = 0
+_KIN_CARTESIAN = 1
+
+
+def resolve_motor_section(config, name, referenced_by):
+    if not config.has_section(name):
+        raise config.error(
+            "%s references motor '%s' but no [%s] section exists"
+            % (referenced_by, name, name)
+        )
+    section = config.getsection(name)
+    drive = section.getchoice("drive", DRIVE_CHOICES, "stepper")
+    return section, drive
+
+
+KINEMATICS_TYPES = {
+    "corexy": [
+        ("a_motors", "axis_x", 0),
+        ("b_motors", "axis_y", 1),
+        ("z_motors", "axis_z", 2),
+    ],
+    "cartesian": [
+        ("x_motors", "axis_x", 0),
+        ("y_motors", "axis_y", 1),
+        ("z_motors", "axis_z", 2),
+    ],
+}
+
+
+def load_kinematics(config, motion):
+    if config.getsection("printer").get("kinematics", None) is not None:
+        raise config.error(
+            "[printer] kinematics is not supported: declare a [kinematics] "
+            "section (type + axis role bindings + motor lists)"
+        )
+    if not config.has_section("kinematics"):
+        raise config.error("[kinematics] section is required")
+    section = config.getsection("kinematics")
+    kind = section.get("type")
+    if kind not in KINEMATICS_TYPES:
+        raise config.error(
+            "[kinematics] type '%s' is not supported (supported: %s)"
+            % (kind, ", ".join(sorted(KINEMATICS_TYPES)))
+        )
+    return _LinearKinematics(config, motion, kind, KINEMATICS_TYPES[kind])
+
+
+class _LinearKinematics:
+    supports_dual_carriage = False
+
+    def __init__(self, config, motion, kind, role_specs):
+        self._motion = motion
+        self.kind = kind
+        self._role_specs = role_specs
+        self._printer = config.get_printer()
+        section = config.getsection("kinematics")
+
+        self._lanes = self._read_lanes(config, section)
+        self.rails = [self._build_lane(config, lane) for lane in self._lanes]
+        self.limits = [(1.0, -1.0)] * 3
+
+        self._printer.load_object(config, "homing").resolve_endstops()
+        self._printer.register_event_handler(
+            "stepper_enable:motor_off", self._handle_motor_off
+        )
+
+    def _read_lanes(self, config, section):
+        lanes = []
+        for role_motors_key, axis_role_key, lane_idx in self._role_specs:
+            axis_name = section.get(axis_role_key)
+            if not config.has_section("axis " + axis_name):
+                raise config.error(
+                    "[kinematics] %s binds to axis '%s' but no [axis %s] "
+                    "section exists" % (axis_role_key, axis_name, axis_name)
+                )
+            motor_names = section.getlist(role_motors_key, [])
+            if not motor_names:
+                raise config.error(
+                    "[kinematics] %s declares no motors (lane %d needs at "
+                    "least one motor)" % (role_motors_key, lane_idx)
+                )
+            lanes.append((lane_idx, axis_name, motor_names))
+        lanes.sort(key=lambda lane: lane[0])
+        return lanes
+
+    def _build_lane(self, config, lane):
+        lane_idx, axis_name, motor_names = lane
+        role_motors_key = self._role_specs[lane_idx][0]
+        referenced_by = "[kinematics] %s" % role_motors_key
+        motor_sections = []
+        drives = set()
+        for motor_name in motor_names:
+            motor_section, drive = resolve_motor_section(
+                config, motor_name, referenced_by
+            )
+            motor_sections.append(motor_section)
+            drives.add(drive)
+        if len(drives) > 1:
+            raise config.error(
+                "%s mixes stepper and servo motors in one lane; a lane must "
+                "be all-stepper or all-servo" % referenced_by
+            )
+        drive = drives.pop()
+        if drive == "servo":
+            return self._build_servo_lane(config, lane, motor_sections)
+        rail = stepper.AxisRail(
+            config.getsection("axis " + axis_name), motor_sections
+        )
+        rail.setup_itersolve(
+            "cartesian_stepper_alloc", "xyz"[lane_idx].encode()
+        )
+        return rail
+
+    def _build_servo_lane(self, config, lane, motor_sections):
+        raise config.error(
+            "servo drive lanes are not yet supported in [kinematics] "
+            "(stepper lanes only for now)"
+        )
+
+    def _handle_motor_off(self, print_time):
+        self.clear_homing_state((0, 1, 2))
+
+    def claimed_axes(self):
+        return [axis_name for _, axis_name, _ in self._lanes]
+
+    def lanes(self):
+        return self._lanes
+
+    def coupled_xy(self):
+        return self.kind == "corexy"
+
+    def mcu_tag(self, lanes_on_mcu):
+        on_mcu = set(lanes_on_mcu)
+        if self.coupled_xy() and 0 in on_mcu and 1 in on_mcu:
+            return _KIN_COREXY
+        return _KIN_CARTESIAN
+
+    def get_steppers(self):
+        return [s for rail in self.rails for s in rail.get_steppers()]
+
+    def active_rails(self, dx, dy, dz):
+        moved = {
+            axis: abs(delta) > 1e-9 for axis, delta in zip("xyz", (dx, dy, dz))
+        }
+        coupled = dict(moved)
+        if self.coupled_xy():
+            coupled["x"] = coupled["y"] = moved["x"] or moved["y"]
+        active = []
+        for lane_idx, _, _ in self._lanes:
+            if coupled["xyz"[lane_idx]]:
+                active.append(self.rails[lane_idx])
+        return active
+
+    def calc_position(self, stepper_positions):
+        def rail_pos(rail):
+            vals = [
+                stepper_positions.get(s.get_name(), 0.0)
+                for s in rail.get_steppers()
+            ]
+            if not vals:
+                return 0.0
+            return sum(vals) / len(vals)
+
+        return [rail_pos(rail) for rail in self.rails]
+
+    def _check_endstops(self, move):
+        end_pos = move.end_pos
+        for i in (0, 1, 2):
+            if move.axes_d[i] and (
+                end_pos[i] < self.limits[i][0] or end_pos[i] > self.limits[i][1]
+            ):
+                if self.limits[i][0] > self.limits[i][1]:
+                    raise move.move_error("Must home axis first")
+                raise move.move_error()
+
+    def check_move(self, move):
+        limits = self.limits
+        xpos, ypos = move.end_pos[:2]
+        if (
+            xpos < limits[0][0]
+            or xpos > limits[0][1]
+            or ypos < limits[1][0]
+            or ypos > limits[1][1]
+        ):
+            self._check_endstops(move)
+        if not move.axes_d[2]:
+            return
+        self._check_endstops(move)
+        z_ratio = move.move_d / abs(move.axes_d[2])
+        move.limit_speed(
+            self._motion._axis_limit("z", "max_velocity") * z_ratio,
+            self._motion._axis_limit("z", "max_accel") * z_ratio,
+        )
+
+    def set_position(self, newpos, homing_axes=()):
+        self._motion.bridge.set_position(newpos[0], newpos[1], newpos[2])
+        for axis in homing_axes:
+            self.limits[axis] = self.rails[axis].get_range()
+
+    def note_z_not_homed(self):
+        self.clear_homing_state([2])
+
+    def clear_homing_state(self, axes):
+        for i in (0, 1, 2):
+            if i in axes:
+                self.limits[i] = (1.0, -1.0)
+
+    def get_status(self, eventtime):
+        from . import gcode as gcode_mod
+
+        x_min, x_max = self.rails[0].get_range()
+        y_min, y_max = self.rails[1].get_range()
+        z_min, z_max = self.rails[2].get_range()
+        homed = "".join(
+            a
+            for i, a in enumerate("xyz")
+            if self.limits[i][0] <= self.limits[i][1]
+        )
+        return {
+            "homed_axes": homed,
+            "axis_minimum": gcode_mod.Coord(x_min, y_min, z_min, 0.0),
+            "axis_maximum": gcode_mod.Coord(x_max, y_max, z_max, 0.0),
+        }
---
+++ test/test_motion_kinematics.py
@@ -0,0 +1,374 @@
+import pytest
+
+from klippy import motion_kinematics
+
+
+class FakeError(Exception):
+    pass
+
+
+class FakePinParams:
+    def __init__(self, pin, chip):
+        self.pin = pin
+        self.chip = chip
+
+    def __getitem__(self, key):
+        return {"pin": self.pin, "invert": False, "chip": self.chip}[key]
+
+
+class FakeMCUEndstop:
+    def __init__(self, pin):
+        self.pin = pin
+        self.steppers = []
+
+    def add_stepper(self, stepper):
+        self.steppers.append(stepper)
+
+
+class FakePins:
+    def __init__(self, chip):
+        self.chip = chip
+
+    def lookup_pin(self, pin, can_invert=False, can_pullup=False):
+        return FakePinParams(pin, self.chip)
+
+    def setup_pin(self, pin_type, pin_desc):
+        assert pin_type == "endstop"
+        return FakeMCUEndstop(pin_desc)
+
+
+class FakeMCU:
+    def __init__(self, printer):
+        self._printer = printer
+        self._oid = 0
+
+    def create_oid(self):
+        self._oid += 1
+        return self._oid
+
+    def register_config_callback(self, cb):
+        pass
+
+    def get_printer(self):
+        return self._printer
+
+
+class FakeRegistrar:
+    def register_stepper(self, config, mcu_stepper):
+        pass
+
+
+class FakeHoming:
+    def resolve_endstops(self):
+        pass
+
+
+class FakePrinter:
+    def __init__(self):
+        self.mcu = FakeMCU(self)
+        self.pins = FakePins(self.mcu)
+        self._objects = {"pins": self.pins, "homing": FakeHoming()}
+        self.event_handlers = []
+
+    def lookup_object(self, name):
+        return self._objects[name]
+
+    def load_object(self, config, name):
+        return self._objects.setdefault(name, FakeRegistrar())
+
+    def register_event_handler(self, event, handler):
+        self.event_handlers.append((event, handler))
+
+    config_error = FakeError
+
+
+_UNSET = object()
+
+
+class FakeConfig:
+    def __init__(self, printer, sections):
+        self._printer = printer
+        self._sections = sections
+        self.error = FakeError
+
+    def get_printer(self):
+        return self._printer
+
+    def has_section(self, name):
+        return name in self._sections
+
+    def getsection(self, name):
+        if name not in self._sections:
+            raise FakeError("no section [%s]" % name)
+        return FakeSection(self._printer, name, self._sections[name])
+
+
+class FakeSection:
+    def __init__(self, printer, name, values):
+        self._printer = printer
+        self._name = name
+        self._values = values
+        self.error = FakeError
+
+    def get_name(self):
+        return self._name
+
+    def get_printer(self):
+        return self._printer
+
+    def lookup_object(self, name):
+        return self._printer.lookup_object(name)
+
+    def _raw(self, option, default):
+        if option in self._values:
+            return self._values[option]
+        if default is _UNSET:
+            raise FakeError(
+                "Option '%s' missing in [%s]" % (option, self._name)
+            )
+        return default
+
+    def get(self, option, default=_UNSET, note_valid=True):
+        return self._raw(option, default)
+
+    def getfloat(
+        self,
+        option,
+        default=_UNSET,
+        minval=None,
+        maxval=None,
+        above=None,
+        below=None,
+        note_valid=True,
+    ):
+        val = self._raw(option, default)
+        if val is None:
+            return None
+        return float(val)
+
+    def getint(self, option, default=_UNSET, minval=None, note_valid=True):
+        val = self._raw(option, default)
+        if val is None:
+            return None
+        return int(val)
+
+    def getboolean(self, option, default=_UNSET, note_valid=True):
+        val = self._raw(option, default)
+        if val is None or isinstance(val, bool):
+            return val
+        return str(val).strip().lower() in ("1", "true", "yes", "on")
+
+    def getchoice(self, option, choices, default=_UNSET, note_valid=True):
+        val = self._raw(option, default)
+        if val not in choices:
+            raise FakeError(
+                "Choice '%s' for option '%s' in [%s] is not valid"
+                % (val, option, self._name)
+            )
+        return choices[val]
+
+    def getlist(
+        self,
+        option,
+        default=_UNSET,
+        seps=(",",),
+        count=None,
+        note_valid=True,
+    ):
+        val = self._raw(option, default)
+        if val is None:
+            return None
+        if isinstance(val, (list, tuple)):
+            return list(val)
+        return [p.strip() for p in val.split(",") if p.strip()]
+
+    def getlists(
+        self,
+        option,
+        default=_UNSET,
+        seps=(",",),
+        count=None,
+        parser=str,
+        note_valid=True,
+    ):
+        return self._raw(option, default)
+
+
+class FakeBridge:
+    def __init__(self):
+        self.set_position_calls = []
+
+    def set_position(self, x, y, z):
+        self.set_position_calls.append((x, y, z))
+
+
+class FakeMotion:
+    def __init__(self):
+        self.bridge = FakeBridge()
+        self._limits = {
+            ("z", "max_velocity"): 10.0,
+            ("z", "max_accel"): 100.0,
+        }
+
+    def _axis_limit(self, axis, kind):
+        return self._limits[(axis, kind)]
+
+
+def motor_section(**extra):
+    values = {
+        "step_pin": "PF0",
+        "dir_pin": "PF1",
+        "rotation_distance": 40.0,
+        "microsteps": 16,
+    }
+    values.update(extra)
+    return values
+
+
+def axis_section(**extra):
+    values = {
+        "position_min": 0.0,
+        "position_max": 300.0,
+        "position_endstop": 0.0,
+        "endstop_pin": "^PE5",
+        "homing_speed": 50.0,
+    }
+    values.update(extra)
+    return values
+
+
+def corexy_sections():
+    return {
+        "printer": {},
+        "kinematics": {
+            "type": "corexy",
+            "axis_x": "x",
+            "axis_y": "y",
+            "axis_z": "z",
+            "a_motors": "motor_a",
+            "b_motors": "motor_b",
+            "z_motors": "motor_z0, motor_z1",
+        },
+        "axis x": axis_section(),
+        "axis y": axis_section(),
+        "axis z": axis_section(position_max=200.0),
+        "motor_a": motor_section(),
+        "motor_b": motor_section(),
+        "motor_z0": motor_section(),
+        "motor_z1": motor_section(),
+    }
+
+
+def cartesian_sections():
+    return {
+        "printer": {},
+        "kinematics": {
+            "type": "cartesian",
+            "axis_x": "x",
+            "axis_y": "y",
+            "axis_z": "z",
+            "x_motors": "motor_x",
+            "y_motors": "motor_y",
+            "z_motors": "motor_z",
+        },
+        "axis x": axis_section(),
+        "axis y": axis_section(),
+        "axis z": axis_section(position_max=200.0),
+        "motor_x": motor_section(),
+        "motor_y": motor_section(),
+        "motor_z": motor_section(),
+    }
+
+
+def make_kin(sections):
+    printer = FakePrinter()
+    config = FakeConfig(printer, sections)
+    motion = FakeMotion()
+    return motion_kinematics.load_kinematics(config, motion)
+
+
+def test_corexy_section_parses_roles_and_motors():
+    kin = make_kin(corexy_sections())
+    assert kin.kind == "corexy"
+    assert kin.claimed_axes() == ["x", "y", "z"]
+    assert kin.lanes()[0] == (0, "x", ["motor_a"])
+    assert kin.lanes()[1] == (1, "y", ["motor_b"])
+    assert kin.lanes()[2] == (2, "z", ["motor_z0", "motor_z1"])
+    assert len(kin.rails) == 3
+    assert len(kin.rails[2].get_steppers()) == 2
+
+
+def test_cartesian_uses_xyz_motor_roles():
+    kin = make_kin(cartesian_sections())
+    assert kin.kind == "cartesian"
+    assert kin.claimed_axes() == ["x", "y", "z"]
+    assert kin.lanes()[0] == (0, "x", ["motor_x"])
+    assert kin.lanes()[1] == (1, "y", ["motor_y"])
+    assert kin.lanes()[2] == (2, "z", ["motor_z"])
+
+
+def test_mcu_tag_corexy_needs_both_xy():
+    corexy = make_kin(corexy_sections())
+    assert corexy.mcu_tag([0, 1, 3]) == 0
+    assert corexy.mcu_tag([2]) == 1
+    assert corexy.mcu_tag([0]) == 1
+    cartesian = make_kin(cartesian_sections())
+    assert cartesian.mcu_tag([0, 1, 2]) == 1
+
+
+def test_coupled_xy_true_for_corexy_false_for_cartesian():
+    assert make_kin(corexy_sections()).coupled_xy() is True
+    assert make_kin(cartesian_sections()).coupled_xy() is False
+
+
+def test_unknown_type_rejected():
+    sections = corexy_sections()
+    sections["kinematics"]["type"] = "hybrid_corexy"
+    with pytest.raises(FakeError) as exc:
+        make_kin(sections)
+    assert "cartesian" in str(exc.value)
+    assert "corexy" in str(exc.value)
+
+
+def test_role_binding_to_undeclared_axis_rejected():
+    sections = corexy_sections()
+    sections["kinematics"]["axis_x"] = "w"
+    with pytest.raises(FakeError):
+        make_kin(sections)
+
+
+def test_missing_kinematics_section_rejected():
+    sections = corexy_sections()
+    del sections["kinematics"]
+    with pytest.raises(FakeError):
+        make_kin(sections)
+
+
+def test_printer_kinematics_key_rejected():
+    sections = corexy_sections()
+    sections["printer"]["kinematics"] = "corexy"
+    with pytest.raises(FakeError) as exc:
+        make_kin(sections)
+    assert "kinematics" in str(exc.value)
+
+
+def test_lane_without_motors_rejected():
+    sections = corexy_sections()
+    sections["kinematics"]["a_motors"] = ""
+    with pytest.raises(FakeError):
+        make_kin(sections)
+
+
+def test_active_rails_couples_xy_for_corexy():
+    kin = make_kin(corexy_sections())
+    active = kin.active_rails(1.0, 0.0, 0.0)
+    assert kin.rails[0] in active
+    assert kin.rails[1] in active
+    assert kin.rails[2] not in active
+
+
+def test_active_rails_independent_for_cartesian():
+    kin = make_kin(cartesian_sections())
+    active = kin.active_rails(1.0, 0.0, 0.0)
+    assert kin.rails[0] in active
+    assert kin.rails[1] not in active
