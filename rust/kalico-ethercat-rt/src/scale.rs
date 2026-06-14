13fe3222c06d10ec8fc5a73ea96091df2c2e7419
Danila Dergachev <dderg@protonmail.com>
2026-06-14 19:27:40 +0200

    feat(ethercat): CountMap::actual_mm + velocity_mm_s conversions

--- rust/kalico-ethercat-rt/src/scale.rs
+++ rust/kalico-ethercat-rt/src/scale.rs
@@ -18,6 +18,15 @@ impl CountMap {
         let delta = (pos_mm - self.origin_mm) * self.counts_per_mm;
         self.origin_counts + delta.round() as i32
     }
+
+    pub fn actual_mm(&self, actual_counts: i32) -> f64 {
+        self.origin_mm
+            + (f64::from(actual_counts) - f64::from(self.origin_counts)) / self.counts_per_mm
+    }
+}
+
+pub fn velocity_mm_s(rpm: i32, rotation_distance: f64) -> f64 {
+    (f64::from(rpm) / 60.0) * rotation_distance
 }
 
 #[cfg(test)]
