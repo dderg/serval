4d8450774ee02859befb0c7c52f86b711970ce89
Danila Dergachev <dderg@protonmail.com>
2026-07-19 17:07:41 +0200

    Revert "Disable frame-timing drive parking unless late_tolerance_us is configured"
    This reverts commit ae1f06866187ca0883ec51146abaf8888fba4d61.

--- klippy/extras/ethercat_node.py
+++ klippy/extras/ethercat_node.py
@@ -79,9 +79,7 @@ class EtherCatNode:
                 % (self.name, self.cycle_us, CYCLE_US_QUANTUM)
             )
         self.dynamics_profile = servo_axis.read_dynamics_profile_option(config)
-        self.late_tolerance_us = config.getfloat(
-            "late_tolerance_us", None, minval=0.0
-        )
+        self.late_tolerance_us = config.getfloat("late_tolerance_us", 0.0)
         self.engine_handle = None
         self._counts_per_mm = None
         self._slot_by_motor = {}
