871201541bad6164670316974996421c5ad53dc9
Danila Dergachev <dderg@protonmail.com>
2026-08-13 02:30:14 +0200

    Keep two seconds of homing runway

--- rust/motion-core/src/pump/drip.rs
+++ rust/motion-core/src/pump/drip.rs
@@ -2,7 +2,7 @@ use super::{AxisKey, AxisQueue};
 use std::collections::{BTreeMap, BTreeSet};
 use std::time::{Duration, Instant};
 
-pub const DRIP_WINDOW_SECS: f64 = crate::anchor::DEFAULT_LEAD_SECS;
+pub const DRIP_WINDOW_SECS: f64 = 2.0;
 
 pub struct DripArm {
     pub cohort: u64,
